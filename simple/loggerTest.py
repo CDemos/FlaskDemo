de8ac3e06616bf12cce560a663dc3a2bd09e600d
Jayin Ton <tonjayin@gmail.com>
2014-07-17 20:41:34 +0800

    logger

--- simple/loggerTest.py
+++ simple/loggerTest.py
@@ -12,25 +12,9 @@ port = 8000
 
 @app.route('/')
 def index():
-    return 'welcome'
-
-
-if __name__ == '__main__':
-    app.run(host=host, port=port, debug=True)
-#!/usr/bin/env python
-# coding=utf-8
-__author__ = 'Jayin Ton'
-
-from flask import Flask
-
-app = Flask(__name__)
-
-host = '127.0.0.1'
-port = 8000
-
-
-@app.route('/')
-def index():
+    app.logger.debug('A value for debugging')
+    app.logger.warning('A warning occurred (%d apples)', 42)
+    app.logger.error('An error occurred')
     return 'welcome'
 
 
